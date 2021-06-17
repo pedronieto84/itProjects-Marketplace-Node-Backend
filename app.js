8b863ccfbe0afe686ccb6205b31af2b7e82ff67f
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-06-17 08:42:02 +0100

    First commit.

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const express = require('express');
 const app = express();
 const db = require('./db'); 
 var UserController = require('./user/UserController');
+// dummi comment
 app.use('/users', UserController);
 module.exports = app;
